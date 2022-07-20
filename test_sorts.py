8291fb6012fd5ba796676c6254f064fa733040d0
RobSeSa <robertssato@gmail.com>
2022-07-19 17:49:47 -0700

    added argparse

--- test_sorts.py
+++ test_sorts.py
@@ -1,11 +1,18 @@
 # test all the sorting functions
 
+import argparse
 from random import randint
 import sort
 
+# parse arguments to see which sort to run
+parser = argparse.ArgumentParser("simple_example")
+parser.add_argument("length", help="Number of elements in the array to be sorted", type=int)
+args = parser.parse_args()
+print(args.length + 1)
+
 
 for _ in range(1):
-    arr = [randint(0, 100) for _ in range(5)]
+    arr = [randint(0, 100) for _ in range(args.length)]
     copy = arr[:]
     print("Input arr:", copy)
     sort.rand(arr)
